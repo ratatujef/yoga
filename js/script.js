d54a2a071a939182188aeefb19a51df9eb088443
ratatujef <ratatujef@gmail.com>
2020-06-29 17:42:17 +0300

    XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
   myTabs(tabs, tabswrap, tabContents);
 
-  let deadLine = "2020-06-25";
+  let deadLine = "2020-06-30";
 
   let timer = document.getElementById("timer"),
     timerHours = timer.querySelector(".hours"),
@@ -42,104 +42,97 @@ window.addEventListener("DOMContentLoaded", function () {
   setInterval(timerRemaining, 1000);
 
 
-function myTabs(tab, tabWrapper, tabContent) {
-  function hideTab(a) {
-    for (let i = a; i < tabContent.length; i++) {
-      tabContent[i].classList.remove("show"); // Класс show берется из верстки, отвечает за видимость блока
-      tabContent[i].classList.add("hide"); // Класс hide берется из верстки, отвечает за скрытие блока
+  function myTabs(tab, tabWrapper, tabContent) {
+    function hideTab(a) {
+      for (let i = a; i < tabContent.length; i++) {
+        tabContent[i].classList.remove("show"); // Класс show берется из верстки, отвечает за видимость блока
+        tabContent[i].classList.add("hide"); // Класс hide берется из верстки, отвечает за скрытие блока
+      }
     }
-  }
-  hideTab(1);
-  function showTab(b) {
-    if (tabContent[b].classList.contains("hide")) {
-      tabContent[b].classList.remove("hide");
-      tabContent[b].classList.add("show");
+    hideTab(1);
+    function showTab(b) {
+      if (tabContent[b].classList.contains("hide")) {
+        tabContent[b].classList.remove("hide");
+        tabContent[b].classList.add("show");
+      }
     }
-  }
-  tabWrapper.addEventListener("click", function (e) {
-    let target = e.target;
-    if (target && target.classList.contains("info-header-tab")) {
-      for (let i = 0; i < tab.length; i++) {
-        if (target == tab[i]) {
-          hideTab(0);
-          showTab(i);
+    tabWrapper.addEventListener("click", function (e) {
+      let target = e.target;
+      if (target && target.classList.contains("info-header-tab")) {
+        for (let i = 0; i < tab.length; i++) {
+          if (target == tab[i]) {
+            hideTab(0);
+            showTab(i);
+          }
         }
       }
-    }
-  });
-}
-let overlay= document.querySelector('.overlay'),
-      close=document.querySelector('.popup-close'),
-      btn=document.querySelector('.more'),
-      miniBtn=document.querySelectorAll('.description-btn');
-   
+    });
+  }
+  let overlay= document.querySelector('.overlay'),
+        close=document.querySelector('.popup-close'),
+        btn=document.querySelector('.more'),
+        miniBtn=document.querySelectorAll('.description-btn');
+    
 
-      for (let i=0; i<miniBtn.length; i++){
-        miniBtn[i].addEventListener('click',()=>{
-          overlay.style.display='block';
+        for (let i=0; i<miniBtn.length; i++){
+          miniBtn[i].addEventListener('click',()=>{
+            overlay.style.display='block';
+            document.body.style.overflow = 'hidden';
+          });
+        }
+
+
+        btn.addEventListener('click', ()=>{
+          overlay.style.display='block'; 
           document.body.style.overflow = 'hidden';
+        
         });
-      }
+        close.addEventListener('click', ()=>{
+          overlay.style.display='none'; 
+          document.body.style.overflow = '';
+        });
 
 
-      btn.addEventListener('click', ()=>{
-        overlay.style.display='block'; 
-        document.body.style.overflow = 'hidden';
+      //form
+
+    let myform=document.querySelector('.main-form'),
+        input=document.getElementsByTagName('input'),
+        statusMessage=document.createElement('div'),
+        message= {
+          loading:'Загрузка',
+          success:'Спасибо, мы свяжеся с вами в ближайшее время!',
+          failure:'Произошла ошибка!'
+        };
+        statusMessage.classList.add('status');
+        myform.addEventListener('submit', function(e){
+        e.preventDefault();
+        myform.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
       
-      });
-      close.addEventListener('click', ()=>{
-        overlay.style.display='none'; 
-        document.body.style.overflow = '';
-      });
+        let formData= new FormData(myform),
+        obj={};
+        formData.forEach((value, key) => {
+          obj[key]=value;
+        });
+        let json=JSON.stringify(obj);
 
+        request.send(json);
+        request.addEventListener('readystatechange', ()=>{
+          if(request.readyState===4&&request.status==200){
+            statusMessage.innerHTML=message.success;
+          } else if (request.readyState<4){
+            statusMessage.innerHTML=message.loading;
+          }else{
+            statusMessage.innerHTML=message.failure;
+          }
 
-// по уроку
+        });
+        for (let i=0; i<input.length; i++){
+          input[i].value='';
+        }
 
-// let deadLine = "2020-06-20";
-
-// function timerRemaining(endTime) {
-//   let t = new Date(Date.parse(endTime) - Date.parse(new Date())),
-//     day = t / 1000 / 60,
-//     hours = t.getHours(t / 1000 / 60 / 60),
-//     min = Math.floor((t / 100 / 60) % 60),
-//     sec = Math.floor((t / 1000) % 60);
-//   return {
-//     total: t,
-//     hour: hours,
-//     minutes: min,
-//     seconds: sec,
-//   };
-// }
-
-// function setClock(endTime, id) {
-//   let timer = document.getElementById(id),
-//     hours = timer.querySelector(".hours"),
-//     minutes = timer.querySelector(".minutes"),
-//     seconds = timer.querySelector(".seconds"),
-//     timeInterval = setInterval(updateClock, 1000);
-
-//   function updateClock() {
-//     let t = timerRemaining(endTime);
-
-//     function getZero(a) {
-//       if (a < 10) {
-//         return "0" + a;
-//       } else {
-//         return a;
-//       }
-//     }
-//     hours.textContent = getZero(t.hour);
-//     minutes.textContent = getZero(t.minutes);
-//     seconds.textContent = getZero(t.seconds);
-
-//     if (t.total <= 0) {
-//       hours.textContent = "00";
-//       minutes.textContent = "00";
-//       seconds.textContent = "00";
-//       clearInterval(timeInterval);
-//     }
-//   }
-// }
-
-// setClock(deadLine, "timer");
+          });
 });
